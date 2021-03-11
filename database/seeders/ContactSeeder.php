0e1ce3908c37bac032e5aee65836d6ddf3b93088
qudi <shintarou0327@yahoo.co.jp>
2021-03-11 16:12:08 +0900

    commit

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'id' => 3,
+            'name' => 'taro',
+            
+        ];
+        DB::table('address')->insert($param);
+    }
+}
